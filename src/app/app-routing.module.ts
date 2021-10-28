b8da0f20f34ae5080a7574308efe780f30dbf7b2
Fares <fares@somewhere.com>
2021-10-28 16:00:55 +0200

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListEndpointComponent } from './list-endpoint/list-endpoint.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "endpoints", component: ListEndpointComponent},
+  { path: "**", component: ListEndpointComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
